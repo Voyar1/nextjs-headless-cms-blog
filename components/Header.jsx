a12462a9742901e33fb2e94b9e2409542d04a3e5
Arkadiusz <areq12x@gmail.com>
2023-01-31 19:53:31 +0100

    added PostCard and PostWidgeto homepage

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,18 +8,18 @@ const categories = [
 const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
+      <div className="border-b w-full flex justify-between items-center border-blue-400 py-8">
+        <div>
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
               VOYAR
             </span>
           </Link>
         </div>
-        <div className="hidden md:float-left md:contents">
+        <div className="hidden md:block">
           {categories.map((category) => (
             <Link key={category.slug} href={`/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <span className="mt-2 text-white ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,11 +14,7 @@ export default function Home({ posts }) {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => {
-            return (
-              <div>
-                <PostCard post={post} key={post.title} />
-              </div>
-            );
+            return <PostCard post={post.node} key={post.node.title} />;
           })}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -34,7 +30,6 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
-
   return {
     props: { posts },
   };
