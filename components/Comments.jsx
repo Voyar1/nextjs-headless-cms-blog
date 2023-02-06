a00523fa3a7a7c6cd727d06a4a1042eb332e4ffe
Arkadiusz <areq12x@gmail.com>
2023-02-06 20:57:16 +0100

    added comments section, FeaturedPosts section and Category page

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,10 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import parse from "html-react-parser";
+import { getComments } from "@/services";
 
 const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result));
+  }, []);
+
   return (
-    <div className="bg-white">
-      <h2>Comments</h2>
-    </div>
+    <>
+      {comments.length > 0 && (
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments</h3>
+          {comments.length} {comments.length === 1 ? "Comment" : "Comments"}
+          {comments.map((comment) => {
+            return (
+              <div
+                key={comment.createdAt}
+                className="border border-gray-100 mb-4 p-2 mt-4 rounded-lg bg-gray-200"
+              >
+                <p className="mb-4">
+                  <span className="font-semibold">{comment.name}</span> on{" "}
+                  {moment(comment.createdAt).format("MMM DD, YYYY")}
+                </p>
+                <p className="whitespace-pre-line text-gray-600 w-full">
+                  {parse(comment.comment)}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -7,9 +7,17 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from "@/components";
+import { useRouter } from "next/router";
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -47,6 +55,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   };
 }
